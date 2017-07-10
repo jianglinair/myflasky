8b1d2dab7895653edf2f91a15d24154ca91da8c9
jianglin <jianglin@qiyi.com>
2017-07-10 21:29:04 +0800

    Example 4-2
        Use redirect and session.
    
    Change-Id: Ia5fec8e417560ad178698767ee051aefb937b97a

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
+# "flask_wtf.Form" has been renamed to "FlaskForm" and will be removed in 1.0.
+# from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 # Required si going away in WTForms 3.0, use DataRequired
 # from wtforms.validators import Required
@@ -18,7 +20,7 @@ from wtforms.validators import DataRequired
 #
 # StringField 构造函数中的可选参数 validators 指定一个由验证函数组成的列表,在接受
 # 用户提交的数据之前验证数据。验证函数 Required() 确保提交的字段不为空。
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
@@ -33,12 +35,11 @@ moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
