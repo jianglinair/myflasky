477c52106bafa2ca5bd920c668c37bda35c7c514
jianglin <jianglin@qiyi.com>
2017-07-16 14:29:47 +0800

    Example 5-1
        Database and SQLAlchemy
    
    Change-Id: Ie209bd9b9572da84fe0616254f4923e94b839391

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
+import os
 from datetime import datetime
 
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
+from flask_script import Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 # "flask_wtf.Form" has been renamed to "FlaskForm" and will be removed in 1.0.
@@ -11,6 +13,32 @@ from wtforms import StringField, SubmitField
 # Required si going away in WTForms 3.0, use DataRequired
 # from wtforms.validators import Required
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+
+# SECRET_KEY is used by Flask-WTF
+app.config['SECRET_KEY'] = 'a secret string hard to guess'
+
+# SQLALCHEMY_DATABASE_URI 指定数据库文件
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+                                        os.path.join(basedir, 'data.sqlite')
+# SQLALCHEMY_COMMIT_ON_TEARDOWN 设为True 时表示每次请求结束后自动提交数据库中的变动
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)  # db 对象是 SQLAlchemy 类的实例,表示程序使用的数据库，
+# 同时还获得了 Flask-SQLAlchemy提供的所有功能
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
 
 
 # 这个表单中的字段都定义为类变量,类变量的值是相应字段类型的对象。在这个示例中,
@@ -25,24 +53,58 @@ class NameForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-app = Flask(__name__)
-# SECRET_KEY is used by Flask-WTF
-app.config['SECRET_KEY'] = 'a secret string hard to guess'
-manager = Manager(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+# 模型这个术语表示程序使用的持久化实体。在 ORM 中,模型一般是一个 Python 类,类中
+# 的属性对应数据库表中的列。
+#
+# Flask-SQLAlchemy 创建的数据库实例为模型提供了一个基类以及一系列辅助类和辅助函数,
+# 可用于定义模型的结构。roles 表和 users 表可定义为模型 Role 和 User
+class Role(db.Model):
+    # 类变量定义在类中且在<函数体>之外。类变量通常不作为实例变量使用。类变量在整个实例化的对象中是公用的。
+    # 类变量 __tablename__ 定义在数据库中使用的表名。
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # 添加到 Role 模型中的 users 属性代表这个关系的面向对象视角。
+    # 对于一个 Role 类的实例,其 users 属性将返回与角色相关联的用户组成的列表。
+    # db.relationship() 的第一个参数表 明这个关系的另一端是哪个模型。如果模型类尚未定义, 可使用字符串形式指定。
+    #
+    # db.relationship() 中的 backref 参数向 User 模型中添加一个 role 属性, 从而定义反向关系。
+    # 这一属性可替代 role_id 访问 Role 模型, 此时获取的是模型对象, 而不是外键的值。
+    users = db.relationship("User", backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        # %r是一个万能的格式付，它会将后面给的参数原样打印出来，带有类型信息
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    # 关系使用users表中的外键连接了两行。添加到User模型中的role_id列被定义为外键,
+    # 就是这个外键建立起了关系。传给db.ForeignKey()的参数'roles.id'表明,这列的值是
+    # roles表中行的id值。
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @app.route('/user/<name>')
